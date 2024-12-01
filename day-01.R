4fc13d21eb0ff2ae4daf3d642037914a8f8fdbfc
Kari Silvennoinen <kari@kalifi.org>
2024-12-01 21:23:05 +0200

    day 1

---
+++ day-01.R
@@ -0,0 +1,18 @@
+library(readr)
+
+input <- read_table("01-input.txt", col_names = FALSE)
+
+# part 1
+sum(abs(sort(input$X1) - sort(input$X2)))
+
+# part 2
+library(dplyr)
+library(tibble)
+
+x2_tbl <- input$X2 |> 
+  as_tibble() |> 
+  count(value) |> 
+  mutate(score = n * value)
+input$X1 |> 
+  map_int(\(x) x2_tbl$score[match(x, x2_tbl$value)]) |> 
+  sum(na.rm = TRUE)
