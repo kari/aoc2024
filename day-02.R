59e8eaa59abef79c9f1c960131df4bb5871fb8a0
Kari Silvennoinen <kari@kalifi.org>
2024-12-02 17:48:02 +0200

    day 2

--- day-02.R
+++ day-02.R
@@ -1,12 +1,39 @@
 library(readr)
+library(meltr)
+library(purrr)
+library(dplyr)
+
+input <- melt_delim("02-input.txt", delim=" ")
 
 # part 1
-input <- melt_delim("02-input.txt", delim=" ") |> 
+safety <- function(vec) {
+  vec |> diff() |> (\(x) min(abs(x)) >= 1 & max(abs(x)) <= 3 & (all(sign(x) < 0) | all(sign(x) > 0)))()
+}
+
+input |> 
   mutate(value = as.integer(value)) |> 
   unstack(value ~ row) |>
-  map(diff) |>
-  map_lgl(\(x) min(abs(x)) >= 1 & max(abs(x)) <= 3 & (all(sign(x) < 0) | all(sign(x) > 0))) |>
+  map_lgl(safety) |>
   sum(na.rm = TRUE)
 
+# part 2
 
-  
+dampener <- function(vec) {
+  if (safety(vec)) {
+    return(TRUE)
+  } else {
+    for (i in 1:length(vec)) {
+      vec2 <- vec[-c(i)]
+      if (safety(vec2)) {
+        return(TRUE)
+      }
+    }
+  }
+  return(FALSE)
+}
+
+input |> 
+  mutate(value = as.integer(value)) |> 
+  unstack(value ~ row) |>
+  map_lgl(dampener) |>
+  sum(na.rm = TRUE)
