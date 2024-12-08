27a44e196c6b2aa8d6cc162322f54ab21a9b99ab
Kari Silvennoinen <kari@kalifi.org>
2024-12-08 23:34:07 +0200

    day 7 part 2

--- day-07.R
+++ day-07.R
@@ -2,6 +2,7 @@ library(meltr)
 library(dplyr)
 library(stringr)
 library(purrr)
+library(arrangements)
 
 input <- melt_delim("07-input.txt", delim = " ") |>
   mutate(value = case_when(
@@ -13,20 +14,38 @@ input <- melt_delim("07-input.txt", delim = " ") |>
 f <- function(acc, val,  op) {
   if (op == "*") {
     return(acc * val)
-  } else {
+  } else if (op == "+") {
     return(acc + val)
+  } else if (op == "||") {
+    return(acc * 10^str_length(val) + val)
   }
 }
 
+# part 1
 acc <- 0
 for (i in input) {
   result <- i[1]
   values <- i[2:length(i)]
-  n <- 2 ^ (length(values) - 1)
   ops <- c("*", "+")
+  n <- length(ops) ^ (length(values) - 1)
   for (i in seq_len(n)) {
     perm <- rev(as.numeric(intToBits(i)))[-(1:(32 - (length(values) - 1)))] + 1
     if (result == reduce2(values, ops[perm], f)) {
+      acc <- acc + result
+      break
+    }
+  }
+}
+format(acc, scientific = FALSE)
+
+# part 2
+acc <- 0
+for (i in input) {
+  result <- i[1]
+  values <- i[2:length(i)]
+  ops <- c("*", "+", "||")
+  for (perm in permutations(ops, replace = TRUE, k = length(values) - 1, layout = "list")) {
+    if (result == reduce2(values, perm, f)) {
       acc <- acc + result
       break
     }
