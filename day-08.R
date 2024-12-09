bd7982101d4450668c83dc8ec66154dd83afe729
Kari Silvennoinen <kari@kalifi.org>
2024-12-09 10:15:47 +0200

    day 8 part 2

--- day-08.R
+++ day-08.R
@@ -5,10 +5,10 @@ library(arrangements)
 input <- read_lines("08-input.txt") |>
   str_split_fixed("", n = Inf)
 
-antinodes <- matrix(nrow = nrow(input), ncol = ncol(input))
-
 antennas <-  c(LETTERS, letters, 0:9)[c(LETTERS, letters, 0:9) %in% input]
 
+# part 1
+antinodes <- matrix(nrow = nrow(input), ncol = ncol(input))
 for (antenna in antennas) {
   idxs <- which(input == antenna)
   pairs <- permutations(idxs, k = 2)
@@ -26,6 +26,43 @@ for (antenna in antennas) {
     if(a2[1] >= 1 && a2[1] <= nrow(antinodes) && a2[2] >= 1 && a2[2] <= ncol(antinodes)) {
       antinodes[a2[1], a2[2]] <- 1
     }
+  }
+}
+
+print(sum(antinodes, na.rm = TRUE))
+
+# part 2
+antinodes <- matrix(nrow = nrow(input), ncol = ncol(input))
+for (antenna in antennas) {
+  idxs <- which(input == antenna)
+  pairs <- permutations(idxs, k = 2)
+
+  for (i in seq_len(nrow(pairs))) {
+    x <- arrayInd(pairs[i, 1], .dim = dim(input))
+    y <- arrayInd(pairs[i, 2], .dim = dim(input))
+
+    n <- 0
+    while (TRUE) {
+      a <- x + n * (x - y)
+      if(a[1] >= 1 && a[1] <= nrow(antinodes) && a[2] >= 1 && a[2] <= ncol(antinodes)) {
+        antinodes[a[1], a[2]] <- 1
+      } else {
+        break
+      }
+      n <- n + 1
+    }
+
+    n <- 0
+    while (TRUE) {
+      a <- y + n * (y - x)
+      if(a[1] >= 1 && a[1] <= nrow(antinodes) && a[2] >= 1 && a[2] <= ncol(antinodes)) {
+        antinodes[a[1], a[2]] <- 1
+      } else {
+        break
+      }
+      n <- n + 1
+    }
+
   }
 }
 
