2ef092e8927ad44798c07c744348786b58749d8a
Kari Silvennoinen <kari@kalifi.org>
2024-12-11 12:55:14 +0200

    even faster

--- day-11.R
+++ day-11.R
@@ -1,12 +1,14 @@
 library(readr)
 library(stringr)
+library(profvis)
 
 input <- read_lines("11-input.txt") |>
   str_split_fixed(" ", n = Inf) |>
   as.numeric()
 
 # part 1
-for (i in 1:75) {
+# profvis({
+for (i in 1:25) {
   print(i)
   print(length(input))
   i <- 1
@@ -15,12 +17,10 @@ for (i in 1:75) {
     if (v == 0) {
       new_input[i] <- 1
       i <- i + 1
-    } else if (str_length(v) %% 2 == 0) {
-      m <- 10^(str_length(v) / 2)
-      l <- v %/% m
-      r <- v %% m
-      new_input[i] <- l
-      new_input[i + 1] <- r
+    } else if (v > 9 && floor(log10(v) + 1) %% 2 == 0) {
+      m <- 10^(floor(log10(v) + 1) / 2)
+      new_input[i] <- v %/% m
+      new_input[i + 1] <- v %% m
       i <- i + 2
     } else {
       new_input[i] <- 2024 * v
@@ -30,4 +30,5 @@ for (i in 1:75) {
   # print(new_input)
   input <- new_input[1:(i - 1)]
 }
+# })
 print(length(input))
