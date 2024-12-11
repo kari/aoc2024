a083fed52b3562aa1f117a9864cf500ae5bab195
Kari Silvennoinen <kari@kalifi.org>
2024-12-11 12:43:22 +0200

    make it faster

--- day-11.R
+++ day-11.R
@@ -6,8 +6,9 @@ input <- read_lines("11-input.txt") |>
   as.numeric()
 
 # part 1
-for (i in 1:25) {
+for (i in 1:75) {
   print(i)
+  print(length(input))
   i <- 1
   new_input <- rep(NA, 2 * length(input))
   for (v in input) {
@@ -15,8 +16,9 @@ for (i in 1:25) {
       new_input[i] <- 1
       i <- i + 1
     } else if (str_length(v) %% 2 == 0) {
-      l <- as.numeric(str_sub(as.character(v), 1, str_length(v) / 2))
-      r <- as.numeric(str_sub(as.character(v), str_length(v) / 2 + 1, str_length(v)))
+      m <- 10^(str_length(v) / 2)
+      l <- v %/% m
+      r <- v %% m
       new_input[i] <- l
       new_input[i + 1] <- r
       i <- i + 2
@@ -25,6 +27,7 @@ for (i in 1:25) {
       i <- i + 1
     }
   }
+  # print(new_input)
   input <- new_input[1:(i - 1)]
 }
 print(length(input))
