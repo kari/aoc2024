908ac98708a66c2a1c3d2749ffb1d2ec12d85a63
Kari Silvennoinen <kari@kalifi.org>
2024-12-12 23:07:27 +0200

    count sides in one for loop

--- day-12.R
+++ day-12.R
@@ -74,81 +74,29 @@ print(sum(map_vec(regions, \(x) x$area * x$perimeter)))
 for (i in seq_along(regions)) {
   r <- regions[[i]]
   sides <- 0
-  rows <- min(which(regions[[i]]$mask == TRUE, arr.ind = TRUE)[, "row"]):max(which(regions[[i]]$mask == TRUE, arr.ind = TRUE)[, "row"])
-  cols <- min(which(regions[[i]]$mask == TRUE, arr.ind = TRUE)[, "col"]):max(which(regions[[i]]$mask == TRUE, arr.ind = TRUE)[, "col"])
 
-  # top sides
-  for (row in rows) {
-    for (col in cols) {
-      if (is.na(r$mask[row, col])) { # skip if not in region
-        next
-      }
-      if (row > 1 && input[row - 1, col] == r$letter) { # skip if there's same letter on top
-        next
-      }
-      # if there's a same letter on left, can't start a side if
-      # it's top row of data frame or
-      # left was empty on top
-      if (col > 1 && input[row, col - 1] == r$letter && (row == 1 || input[row - 1, col - 1] != r$letter)) {
-        next
-      }
+  for (j in which(regions[[i]]$mask)) {
+    idx <- arrayInd(j, .dim = dim(input))
+    row <- idx[1]
+    col <- idx[2]
+
+    # top sides
+    if (!(row > 1 && input[row - 1, col] == r$letter) && !(col > 1 && input[row, col - 1] == r$letter && (row == 1 || input[row - 1, col - 1] != r$letter))) {
       sides <- sides + 1
     }
-  }
 
-  # left sides
-  for (col in cols) {
-    for (row in rev(rows)) { # note down-to-up
-      if (is.na(r$mask[row, col])) { # skip if not in region
-        next
-      }
-      if (col > 1 && input[row, col - 1] == r$letter) { # skip if there's same letter on left
-        next
-      }
-      # if there's a same letter on below, can't start a side if
-      # it's first col of data frame or
-      # bottom was empty on left
-      if (row < nrow(input) && input[row + 1, col] == r$letter && (col == 1 || input[row + 1, col - 1] != r$letter)) {
-        next
-      }
+    # left sides
+    if (!(col > 1 && input[row, col - 1] == r$letter) && !(row < nrow(input) && input[row + 1, col] == r$letter && (col == 1 || input[row + 1, col - 1] != r$letter))) {
       sides <- sides + 1
     }
-  }
 
-  # right sides
-  for (col in rev(cols)) { # right to left
-    for (row in rows) {
-      if (is.na(r$mask[row, col])) { # skip if not in region
-        next
-      }
-      if (col < ncol(input) && input[row, col + 1] == r$letter) { # skip if there's same letter on right
-        next
-      }
-      # if there's a same letter up, can't start a side if
-      # it's last col of data frame or
-      # up was empty on right
-      if (row > 1 && input[row - 1, col] == r$letter && (col == ncol(input) || input[row - 1, col + 1] != r$letter)) {
-        next
-      }
+    # right sides
+    if (!(col < ncol(input) && input[row, col + 1] == r$letter) && !(row > 1 && input[row - 1, col] == r$letter && (col == ncol(input) || input[row - 1, col + 1] != r$letter))) {
       sides <- sides + 1
     }
-  }
 
-  # bottom sides
-  for (row in rev(rows)) {
-    for (col in cols) {
-      if (is.na(r$mask[row, col])) { # skip if not in region
-        next
-      }
-      if (row < nrow(input) && input[row + 1, col] == r$letter) { # skip if there's same letter on below
-        next
-      }
-      # if there's a same letter on left, can't start a side if
-      # it's last row of data frame or
-      # left was empty on bottom
-      if (col > 1 && input[row, col - 1] == r$letter && (row == nrow(input) || input[row + 1, col - 1] != r$letter)) {
-        next
-      }
+    # bottom sides
+    if (!(row < nrow(input) && input[row + 1, col] == r$letter) && !(col > 1 && input[row, col - 1] == r$letter && (row == nrow(input) || input[row + 1, col - 1] != r$letter))) {
       sides <- sides + 1
     }
   }
